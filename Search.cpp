912c648db83fd0cc2aa8a873600b5234927ad604
ranf999 <1253936027@qq.com>
2016-09-12 23:49:09 -0700

    DFS complete

--- Search.cpp
+++ Search.cpp
@@ -86,8 +86,65 @@ Solution Search::BFS(Problem problem)
 
 Solution Search::DFS(Problem problem)
 {
+	string state;  //
+	int depth = 0;  //depth of search tree
+	vector<int> pathCosts;
 	Solution solution;
-	return solution;
+	vector<string> actions;//results
+	stack<string> tmp;//
+	stack<string> frontier;
+	set<string> explored;
+	unordered_map<string, string> parent;//find parent node
+	vector<Node> successors;
+
+	state = problem.getStartState();
+	actions.push_back(state);
+
+	if (problem.isGoalState(state))
+	{
+		pathCosts.push_back(0);
+		solution.setActions(actions);
+		solution.setCosts(pathCosts);
+		return solution;
+	}
+	frontier.push(state);
+	while (!frontier.empty())
+	{
+		pathCosts.push_back(depth);
+		depth++;
+		state = frontier.top();
+		frontier.pop();
+		if (problem.isGoalState(state))
+		{
+			//build solution
+			pathCosts.push_back(depth);
+			string child = state;
+			while (child.compare(problem.getStartState()) != 0)
+			{
+				tmp.push(child);
+				child = parent[child];
+			}
+			while (!tmp.empty())
+			{
+				actions.push_back(tmp.top());
+				tmp.pop();
+			}
+			solution.setActions(actions);
+			solution.setCosts(pathCosts);
+			return solution;
+		}
+		explored.insert(state);
+		successors = problem.getSuccessors(state);
+		for (int i=successors.size()-1; i>=0 ; i--)
+		{
+			Node node = successors[i];
+			if (explored.count(node.getState()) == 0)
+			{
+				parent[node.getState()] = node.getParent();
+				frontier.push(node.getState());
+			}
+		}
+	}
 }
 
 Solution Search::UCS(Problem problem)
